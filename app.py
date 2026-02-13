00734f42164fa39bb778370658a12210c4027d32
caogiahuyy-wq <cao.giahuyy@gmail.com>
2026-02-13 11:02:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ btn_vi = types.InlineKeyboardButton("🇻🇳", callback_data="lang_vi")
 btn_ja = types.InlineKeyboardButton("🇯🇵", callback_data="lang_ja")
 btn_th = types.InlineKeyboardButton("🇹🇭", callback_data="lang_th")
 btn_zh = types.InlineKeyboardButton("🇨🇳", callback_data="lang_zh")
-markup = types.InlineKeyboardMarkup()
+markup = types.InlineKeyboardMarkup("en", "ru", "ar", "vi", "ja", "th", "zh")
 
 markup.row(btn_en, btn_ru, btn_ar, btn_vi, btn_ja, btn_th, btn_zh)
 
